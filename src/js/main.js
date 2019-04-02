4203c21608934b3458626a1a7e0dfeb116fb43bf
frontepam <frontepam@gmail.com>
2019-04-02 21:28:05 +0300

    no message

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-alert('hello world');
+function test(){
+var eng = ['green', 'yellow', 'red', 'black'];
+var rus = ['зеленый', 'желтый', 'красный', 'черный'];
+
+do{
+var index = Math.floor(Math.random() * (eng.length -1));
+var answer = prompt (eng [index]);
+if (rus [index] == answer){
+
+alert ('Красаучег');
+} else {
+
+    alert (' Иди подучи');
+}
+} while (confirm ('continue?'));
+}
+
+test();
