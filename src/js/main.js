5671292b9f1e23fd4ca4f0b10b00fa6d03320bfa
frontepam <frontepam@gmail.com>
2019-04-13 14:36:51 +0300

    no message

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-alert ('Hello World!')
+var questions = ['First name', 'Last name', 'Gender', 'Age'];
+var answers = [];
+
+for (var i = 0; i < questions.length; i++){
+  
+ answers[i] = prompt ('Введите' + questions[i]); 
+  
+}
+
+alert (answers);
