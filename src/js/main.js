3a59a8d69ddf5ee29f47de620021a8abd1e3ee1e
Dzmitry-ПК\Dzmitry <frontepam@gmail.com>
2019-04-29 23:09:18 +0300

    no message

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-
-var opener = document.querySelector('.burger-nav__opener');
-var opener = document.querySelector('.burger-nav__items');
-
-function toggleBurger(){
-opener.classList.toggle('open');
-items.classList.toggle('open');
-}
-open.addEventListener('click', toggleBurger); 
